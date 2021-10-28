64be2d5288ac768b220f2b961256c7937f2abea5
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-10-28 21:29:33 +0200

    feat: Add products/show route

--- src/models/products.ts
+++ src/models/products.ts
@@ -32,7 +32,32 @@ const index = async (): Promise<Product[]> => {
   }
 };
 
+const show = async (id: string): Promise<Product> => {
+  const sql = `SELECT products.*, categories.name AS "category_name" FROM products INNER JOIN categories ON products.category_id = categories.id WHERE products.id = ${id};`;
+
+  try {
+    const conn = await db_client.connect();
+    const result = await conn.query(sql);
+
+    conn.release();
+
+    return result.rows.map(row => ({
+      id: row.id,
+      name: row.name,
+      price: row.price,
+      category: {
+        id: row.category_id,
+        name: row.category_name
+      }
+    }))[0];
+  }
+  catch (err) {
+    throw new Error(`Could not get products. Error: ${err}`);
+  }
+};
+
 export {
   Product,
-  index
+  index,
+  show
 }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,11 +1,16 @@
 import { Router, Request, Response } from 'express';
-import { index } from '../models/products';
+import { index, show } from '../models/products';
 
 const router = Router();
 
 router.get('/', async (_req: Request, res: Response): Promise<void> => {
   const products = await index();
   res.send(products);
+});
+
+router.get('/:id', async (req: Request, res: Response): Promise<void> => {
+  const product = await show(req.params.id);
+  res.send(product);
 });
 
 export default router;
