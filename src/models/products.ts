924ccdd3865274d2055f9cf827729ab2c62ffbb0
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-10-28 22:06:51 +0200

    feat: Add products/create route

--- src/models/products.ts
+++ src/models/products.ts
@@ -34,6 +34,7 @@ const index = async (): Promise<Product[]> => {
 
 const show = async (id: string): Promise<Product> => {
   const sql = `SELECT products.*, categories.name AS "category_name" FROM products INNER JOIN categories ON products.category_id = categories.id WHERE products.id = ${id};`;
+  console.log(id);
 
   try {
     const conn = await db_client.connect();
@@ -56,8 +57,26 @@ const show = async (id: string): Promise<Product> => {
   }
 };
 
+const create = async (name: string, price: number, category: number): Promise<Category> => {
+  const nameNorm = name.toLowerCase();
+  const sql = `INSERT INTO products (name, price, category_id) VALUES ('${nameNorm}', ${price}, ${category}) RETURNING id;`;
+
+  try {
+    const conn = await db_client.connect();
+    const result = await conn.query(sql);
+
+    conn.release();
+
+    return await show(result.rows[0].id);
+  }
+  catch (err) {
+    throw new Error(`Could not get categories. Error: ${err}`);
+  }
+};
+
 export {
   Product,
   index,
-  show
+  show,
+  create
 }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { index, show } from '../models/products';
+import { index, show, create } from '../models/products';
 
 const router = Router();
 
@@ -11,6 +11,18 @@ router.get('/', async (_req: Request, res: Response): Promise<void> => {
 router.get('/:id', async (req: Request, res: Response): Promise<void> => {
   const product = await show(req.params.id);
   res.send(product);
+});
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+  const { name, price, category } = req.body;
+
+  if (name === undefined || price === undefined || category === undefined) {
+    res.send(`All attributes ('name', 'price', 'category') must be supplied in the body.`);
+  }
+  else {
+    const newProduct = await create(name, price, category);
+    res.send(newProduct);
+  }
 });
 
 export default router;
