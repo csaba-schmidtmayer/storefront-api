b4c78cbaecb74719957098a5af8e71b3d233b0e2
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-10-29 20:13:08 +0200

    feat: Add users/show route

--- src/models/users.ts
+++ src/models/users.ts
@@ -26,7 +26,28 @@ const index = async (): Promise<User[]> => {
   }
 };
 
+const show = async (id: string): Promise<User> => {
+  const sql = `SELECT (id, first_name, last_name) FROM users WHERE id = ${id};`;
+
+  try {
+    const conn = await db_client.connect();
+    const result = await conn.query(sql);
+
+    conn.release();
+
+    return result.rows.map((row) => ({
+      id: row.id,
+      firstName: row.first_name,
+      lastName: row.last_name
+    }))[0];
+  }
+  catch (err) {
+    throw new Error(`Could not get users. Error: ${err}`);
+  }
+};
+
 export {
   User,
-  index
+  index,
+  show
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response } from 'express';
 
-import { index } from '../models/users';
+import { index, show } from '../models/users';
 
 const router = Router();
 
 router.get('/', async (_req: Request, res: Response): Promise<void> => {
   const users = await index();
   res.send(users);
+});
+
+router.get('/:id', async (req: Request, res: Response): Promise<void> => {
+  const { id } = req.params;
+  const user = await show(id);
+  res.send(user);
 });
 
 export default router;
