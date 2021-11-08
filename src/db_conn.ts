10ab4de46a1fd147315279fa6f368c1debef42fb
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-08 20:53:38 +0100

    refactor: Move host and db to .env

--- src/db_conn.ts
+++ src/db_conn.ts
@@ -3,17 +3,19 @@ import { Pool } from 'pg';
 
 dotenv.config()
 const {
-    DEV_USERNAME,
-    DEV_PASSWORD,
-    DEV_PORT
+  DEV_HOST,
+  DEV_DB,
+  DEV_USERNAME,
+  DEV_PASSWORD,
+  DEV_PORT
 } = process.env
 
 const db_client = new Pool({
-    host: 'localhost',
-    database: 'storefront',
-    user: DEV_USERNAME,
-    password: DEV_PASSWORD,
-    port: DEV_PORT ? parseInt(DEV_PORT) : 3000
+  host: DEV_HOST,
+  database: DEV_DB,
+  user: DEV_USERNAME,
+  password: DEV_PASSWORD,
+  port: DEV_PORT ? parseInt(DEV_PORT) : 3000
 });
 
 export default db_client;
--- src/tests/test_db_conn.ts
+++ src/tests/test_db_conn.ts
@@ -3,17 +3,19 @@ import { Pool } from 'pg';
 
 dotenv.config()
 const {
-    TEST_USERNAME,
-    TEST_PASSWORD,
-    TEST_PORT
+  TEST_HOST,
+  TEST_DB,
+  TEST_USERNAME,
+  TEST_PASSWORD,
+  TEST_PORT
 } = process.env
 
 const db_client = new Pool({
-    host: 'localhost',
-    database: 'storefront_test',
-    user: TEST_USERNAME,
-    password: TEST_PASSWORD,
-    port: TEST_PORT ? parseInt(TEST_PORT) : 3000
+  host: TEST_HOST,
+  database: TEST_DB,
+  user: TEST_USERNAME,
+  password: TEST_PASSWORD,
+  port: TEST_PORT ? parseInt(TEST_PORT) : 3000
 });
 
 export default db_client;
