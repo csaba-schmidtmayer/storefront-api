a136d83feb5e195a12ee56c237ab820bfd8c1c0c
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2021-11-08 21:54:50 +0100

    fix: Fix db connection in test environment

--- src/db_conn.ts
+++ src/db_conn.ts
@@ -3,19 +3,27 @@ import { Pool } from 'pg';
 
 dotenv.config()
 const {
+  NODE_ENV,
   DEV_HOST,
   DEV_DB,
   DEV_USERNAME,
   DEV_PASSWORD,
-  DEV_PORT
+  DEV_PORT,
+  TEST_HOST,
+  TEST_DB,
+  TEST_USERNAME,
+  TEST_PASSWORD,
+  TEST_PORT
 } = process.env
 
 const db_client = new Pool({
-  host: DEV_HOST,
-  database: DEV_DB,
-  user: DEV_USERNAME,
-  password: DEV_PASSWORD,
-  port: DEV_PORT ? parseInt(DEV_PORT) : 3000
+  host: NODE_ENV === 'test' ? TEST_HOST : DEV_HOST,
+  database: NODE_ENV === 'test' ? TEST_DB : DEV_DB,
+  user: NODE_ENV === 'test' ? TEST_USERNAME : DEV_USERNAME,
+  password: NODE_ENV === 'test' ? TEST_PASSWORD : DEV_PASSWORD,
+  port: NODE_ENV === 'test'
+    ? TEST_PORT ? parseInt(TEST_PORT) : 3000
+    : DEV_PORT ? parseInt(DEV_PORT) : 3000
 });
 
 export default db_client;
